a8dd6206e0021cefb0c299944fba1cb699aef8cc
Denis Dzieza <d.dzieza@havida-it.com>
2023-10-16 19:59:41 +0200

    add basic layout with tailwind styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="container min-h-screen pt-12 flex flex-col items-center">
+	<slot />
+</main>
+
+<style lang="postcss">
+	:global(body) {
+		background-color: theme(colors.earth.950);
+		color: theme(colors.gray.100);
+	}
+</style>
